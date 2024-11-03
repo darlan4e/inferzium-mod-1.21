7677c3b26931c41a99c78e1e3b493e8654b24317
darlan <denis.darlan@yandex.ru>
2024-11-03 20:01:25 +0300

    added important comment

--- src/main/java/net/darlan/inferziummod/InferziumMod.java
+++ src/main/java/net/darlan/inferziummod/InferziumMod.java
@@ -5,6 +5,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+//very important comment
 public class InferziumMod implements ModInitializer {
 	public static final String MOD_ID = "inferziummod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
