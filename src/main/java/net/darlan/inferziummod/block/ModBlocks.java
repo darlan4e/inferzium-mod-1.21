0bdc54241e831844f3b909270a3c87bdd53bea3b
darlan <denis.darlan@yandex.ru>
2024-11-04 00:31:06 +0300

    added the ability to mine "kikimond ore" and now it drops "raw kikimond"

--- src/main/java/net/darlan/inferziummod/block/ModBlocks.java
+++ src/main/java/net/darlan/inferziummod/block/ModBlocks.java
@@ -4,6 +4,7 @@ import net.darlan.inferziummod.InferziumMod;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
@@ -11,14 +12,16 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 
 public class ModBlocks {
     public static final Block KIKIMOND_BLOCK = registerBlock("kikimond_block",
-            new Block(AbstractBlock.Settings.create().strength(4f, 3f)
+            new Block(AbstractBlock.Settings.create().strength(3f, 3f)
                     .requiresTool().sounds(BlockSoundGroup.METAL)));
     public static final Block KIKIMOND_ORE = registerBlock("kikimond_ore",
-            new Block(AbstractBlock.Settings.create().strength(4f, 3f)
-                    .requiresTool().sounds(BlockSoundGroup.STONE)));
+            new ExperienceDroppingBlock(UniformIntProvider.create(2, 5),
+                    AbstractBlock.Settings.create().strength(2f, 2f)
+                            .requiresTool()));
 
     private static Block registerBlock(String name, Block block){
         registerBlockItem(name, block);
